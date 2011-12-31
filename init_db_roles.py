b806c10d91a3e4003d1d9a1d4658b2036e9b6e4d
Ben Motz <ben@ouat.co.uk>
2011-12-31 16:36:38 +0000

    Refactor to be tidier

--- init_db_roles.py
+++ init_db_roles.py
@@ -6,8 +6,6 @@ import diary.models
 
 FORMATS_PATH="./formats"
 
-def clean(string):
-    return string.strip().replace("_", " ")
 
 def create_event_types(event_types):
     for e_type, roles in event_types.iteritems():
@@ -23,9 +21,12 @@ def create_event_types(event_types):
                 logging.info("Adding role %s to %s", role, e_type)
                 e_type_o.roles.add(role)
 
-def main():
+def load_event_templates(path_to_formats):
+    def clean(string):
+        return string.strip().replace("_", " ")
+
     type_index = {}
-    for root, dirs, files in os.walk(FORMATS_PATH):
+    for root, dirs, files in os.walk(path_to_formats):
         for event_type in files:
             role_list = []
             with open(os.path.join(root, event_type), "r") as role_list_file:
@@ -33,7 +34,11 @@ def main():
                     role_list.append(clean(line))
             type_index[clean(event_type)] = role_list
     logging.info("Loaded %d event types: %s", len(type_index), ",".join(type_index.keys()))
+    return type_index
 
+
+def main():
+    type_index = load_event_templates(FORMATS_PATH)
     create_event_types(type_index)
 
 if __name__ == "__main__":
