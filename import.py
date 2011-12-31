54661887287150c31dad1802ea701cf2781837ef
Ben Motz <ben@ouat.co.uk>
2011-12-31 16:48:01 +0000

    Consolidate event template import into main import file

--- import.py
+++ import.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import os
 import sys
 import datetime
 import MySQLdb
@@ -6,6 +7,8 @@ import re
 import logging
 
 import diary.models
+
+FORMATS_PATH="./formats"
 
 def titlecase(string):
 #   return string.title() # Really doesn't cope with apostrophes.
@@ -198,15 +201,54 @@ def create_roles(connection):
 
     return roles
 
+###############################################################################
+# Create event templates using dict { event_template_name : [ list of role names] }
+def create_event_types(event_types):
+    for e_type, roles in event_types.iteritems():
+        e_type_o, created = diary.models.EventType.objects.get_or_create(name=e_type)
+        if created:
+            logging.info("Created event type: %s", e_type)
+            e_type_o.shortname = e_type
+            e_type_o.save()
+        assigned_roles_keys = set([r['pk'] for r in e_type_o.roles.values('pk')])
+        for role in roles:
+            role = diary.models.Role.objects.get(name=role)
+            if role.pk not in assigned_roles_keys:
+                logging.info("Adding role %s to %s", role, e_type)
+                e_type_o.roles.add(role)
+
+# Read event templates from given path
+def load_event_templates(path_to_formats):
+    def clean(string):
+        return string.strip().replace("_", " ")
+
+    type_index = {}
+    for root, dirs, files in os.walk(path_to_formats):
+        for event_type in files:
+            role_list = []
+            with open(os.path.join(root, event_type), "r") as role_list_file:
+                for line in role_list_file:
+                    role_list.append(clean(line))
+            type_index[clean(event_type)] = role_list
+    logging.info("Loaded %d event types: %s", len(type_index), ",".join(type_index.keys()))
+    return type_index
+
 def main():
+
     conn = connect()
+    # Create roles
     role_map = create_roles(conn)
+    # Create event templates
+    event_templates = load_event_templates(FORMATS_PATH)
+    create_event_types(event_templates)
+
     if role_map is None:
         return
 
     import_events(conn, role_map)
     import_ideas(conn)
     conn.close ()
+
 
 if __name__ == "__main__":
     main()
