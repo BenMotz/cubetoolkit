8f1e3f30cfc90f3e3a4c278479e52f10d4218f9d
Ben Motz <ben@ouat.co.uk>
2012-01-15 00:41:58 +0000

    Add docstring

--- diary/templatetags/hash_filter.py
+++ diary/templatetags/hash_filter.py
@@ -1,4 +1,6 @@
 from django.template.defaultfilters import register
+"""Simple filter for use in django templates that allows extraction of a given
+key from a dictionary"""
 
 @register.filter(name='lookup')
 def lookup(d, key):
