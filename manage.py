46433931be8522d2d4bac360b499fd5bf72efe01
Ben Motz <ben@ouat.co.uk>
2012-02-08 21:41:32 +0000

    Change manage.py not to use bizarre logic for guessing path to site module

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
+from django.core.management import execute_from_command_line # execute_manager
 import imp
 try:
     imp.find_module('settings') # Assumed to be in the same directory.
@@ -8,7 +8,8 @@ except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
 
-import settings
+# import settings
 
 if __name__ == "__main__":
-    execute_manager(settings)
+#    execute_manager(settings)
+    execute_from_command_line()
