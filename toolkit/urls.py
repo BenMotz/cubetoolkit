d9907e0f854c7e3ca8d96025a81ed1988bbbf7a7
Ben Motz <ben@ouat.co.uk>
2012-02-11 23:49:36 +0000

    Change to using static files from apache for dev server

--- toolkit/urls.py
+++ toolkit/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import patterns, include, url
-import django.conf
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -19,6 +18,4 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^static/(.*)$', 'django.views.static.serve',{'document_root':django.conf.settings.STATIC_ROOT}),
-
 )
