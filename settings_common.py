854f28fe7ffc4b26aa923ad8fd5c6896ebc0a904
Ben Motz <ben@ouat.co.uk>
2012-02-18 14:57:39 +0000

    Configure logging

--- settings_common.py
+++ settings_common.py
@@ -155,44 +155,55 @@ INSTALLED_APPS = (
     'django.contrib.markup',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'formatters': {
-#         'verbose': {
-#             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-#         },
-#         'simple': {
-#             'format': '%(levelname)s %(message)s'
-#         },
-#     },
-#     'handlers': {
-#         'console' : {
-#             'level' : 'DEBUG',
-#             'class' : 'logging.StreamHandler',
-#             'formatter' : 'simple',
-#         },
-#         'mail_admins': {
-#             'level': 'ERROR',
-#             'class': 'django.utils.log.AdminEmailHandler'
-#         }
-#     },
-#     'loggers': {
-#         'django.request': {
-#             'handlers': ['mail_admins'],
-#             'level': 'ERROR',
-#             'propagate': True,
-#         },
-#         'toolkit.diary.views' : {
-#             'handlers' : ['console',],
-#             'level' : 'DEBUG',
-#             'propagate' : True,
-#         }
-#     }
-# }
-# logging.config.dictConfig(LOGGING)
+# Sends an email to the site admins on every HTTP 500 error and logs everything
+# to /tmp/toolkit.log.
+# (Configuration on how it sends emails are in settings_* files)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s %(module)s %(funcName)s %(levelname)s : %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console' : {
+            'level' : 'DEBUG',
+            'class' : 'logging.StreamHandler',
+            'formatter' : 'simple',
+        },
+        'file' : {
+            'level' : 'DEBUG',
+            'class' : 'logging.handlers.RotatingFileHandler',
+            'formatter' : 'verbose',
+            'filename' : '/tmp/toolkit.log',
+            'maxBytes' : 2048,
+            'backupCount' : 3,
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins','file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+#        'toolkit' : {
+#            'handlers' : ['console','file'],
+#            'level' : 'DEBUG',
+#            'propagate' : True,
+#        },
+        '' : {
+            'handlers' : ['console','file',],
+            'level' : 'DEBUG',
+            'propagate' : True,
+        },
+    }
+}
+logging.config.dictConfig(LOGGING)
