e9fc27d85b64785ce1be7f6c07200359645d5f22
Ben Motz <ben@ouat.co.uk>
2012-02-18 21:57:09 +0000

    Rename settings files, improve fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 import os
 import os.path
 
-from fabric.api import require, env, run, cd, local, put
+from fabric.api import require, env, run, cd, local, put, sudo
 from fabric import utils
 from fabric.contrib import console
 
@@ -18,7 +18,7 @@ def testing():
     env.user = "ben"
     env.hosts = ["localhost"]
     env.git_source = "ben@phonog.dyndns.org:data/python/cube"
-    env.settings = "settings_testing.py"
+    env.settings = "testing.settings.py"
 
 def production():
     """Configure to deploy live"""
@@ -26,21 +26,36 @@ def production():
     env.site_root = "/home/users/toolkit/site"
     env.user = "cubetoolkit"
     env.hosts = ["toolkit.cubecinema.com"]
-    env.settings = "settings_live.py"
+    env.settings = "live.settings.py"
 
 def deploy_code():
+    """Deploy code from git HEAD onto target"""
     # Check that target is defined:
     require('site_root', provided_by = ('testing', 'production'))
 
     archive = "site_transfer.tgz"
-    # local("git archive --format=tgz HEAD > {0}".format(archive))
-    local('tar -czf {0} toolkit/ --exclude=".pyc"'.format(archive))
+    local("git archive --format=tgz HEAD > {0}".format(archive))
+    # local('tar -czf {0} toolkit/ --exclude=".pyc"'.format(archive))
     put(archive, env.site_root)
     with cd(env.site_root):
         target = os.path.join(env.site_root, CODE_DIR)
         run("rm -rf {0}".format(target))
         run("tar -xzf {0}".format(archive))
-        run("cp {0} settings.py".format(env.settings))
+        run("ln -s {0} settings.py".format(env.settings))
+
+def deploy_static():
+    """Rsync all static content onto target"""
+    # Check that target is defined:
+    require('site_root', provided_by = ('testing', 'production'))
+
+    local('rsync -av --delete static/ {0}@{1}:{2}/static'.format(env.user, env.hosts[0], env.site_root))
+
+def deploy_media():
+    """Rsync all media content onto target"""
+    # Check that target is defined:
+    require('site_root', provided_by = ('testing', 'production'))
+
+    local('rsync -av --delete media/ {0}@{1}:{2}/media'.format(env.user, env.hosts[0], env.site_root))
 
 def update_requirements():
     """ Update installed packages in remote virtualenv """
@@ -52,21 +67,27 @@ def update_requirements():
 
 def restart_server():
     # ??
-    run("sudo apache2ctl restart")
+    sudo("apache2ctl restart")
 
 ## Disabled, for destruction avoidance
-#def bootstrap():
-#    """Wipe out what has gone before, build virtual environment, uppload code"""
-#    # Check that target is defined:
-#    require('site_root', provided_by = ('testing', 'production'))
-#    # Scorch the earth
-#    run("rm -rf %(site_root)s" % env)
-#    deploy_code()
-#    # Create the virtualenv:
-#    venv_path = os.path.join(env.site_root, VIRTUALENV)
-#    run("virtualenv --system-site-packages {0}".format(venv_path))
-#    # Update the packages installed in the environment:
-#    update_requirements()
+def bootstrap():
+    """Wipe out what has gone before, build virtual environment, uppload code"""
+    if not console.confirm("Flatten remote, including media files?", default=False):
+        utils.abort("User aborted")
+    # Check that target is defined:
+    require('site_root', provided_by = ('testing', 'production'))
+    # Scorch the earth
+    run("rm -rf %(site_root)s" % env)
+    # Recreate the directory
+    run("mkdir %(site_root)s" % env)
+    deploy_code()
+    deploy_static()
+    deploy_media()
+    # Create the virtualenv:
+    venv_path = os.path.join(env.site_root, VIRTUALENV)
+    run("virtualenv --system-site-packages {0}".format(venv_path))
+    # Update the packages installed in the environment:
+    update_requirements()
 
 def deploy():
     """Upload code, install any new requirements"""
@@ -74,9 +95,10 @@ def deploy():
     require('site_root', provided_by = ('testing', 'production'))
     if env.target == 'production':
         if not console.confirm("Uploading to live site: sure?", default=False):
-            utils.abort()
+            utils.abort("User aborted")
 
     deploy_code()
+    deploy_static()
     update_requirements()
     restart_server()
 
