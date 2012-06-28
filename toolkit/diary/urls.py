4e1fa9b21398c20233e99384e025d975f3e3eba9
Ben Motz <ben@ouat.co.uk>
2012-06-28 23:05:30 +0100

    Change JSON url to be consistent with HTML urls

--- toolkit/diary/urls.py
+++ toolkit/diary/urls.py
@@ -60,7 +60,7 @@ urlpatterns = patterns( 'toolkit.diary.views',
 
     # Ajax calls:
     url("^edit/setprefs$", 'set_edit_preferences', name="set_edit_preferences"),
-    url('^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/json$', 'view_diary_json', name="day-view-json"),
+    url('^view/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/json$', 'view_diary_json', name="day-view-json"),
 
 )
 
