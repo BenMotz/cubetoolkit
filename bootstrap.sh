2eac6784142eec0008b8dde575c0ab0843333ac4
Ben Motz <ben@ouat.co.uk>
2012-09-05 18:56:12 +0100

    Add hashbang, fix pip invocation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Trivial script to create virtualenv and do initial package install
 # (Requires system pip to be available)
 VENV_PATH=./venv
@@ -5,4 +6,4 @@ REQUIREMENTS_FILE=requirements.txt
 
 virtualenv $VENV_PATH --system-site-packages
 source $VENV_PATH/bin/activate
-pip install -E $VENV_PATH --requirement $REQUIREMENTS_FILE
+pip install --requirement $REQUIREMENTS_FILE
