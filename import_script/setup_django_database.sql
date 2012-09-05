5193264ba9686a2f75433dde182742e3284eea03
Ben Motz <ben@ouat.co.uk>
2012-09-05 20:41:31 +0100

    Straighten out the somewhat wonky database setup script

--- import_script/setup_django_database.sql
+++ import_script/setup_django_database.sql
@@ -1,27 +1,26 @@
 # Do some dummy grants, which will create the users if they didn't already
 # exist, so that the following DROP USER won't give an error:
 GRANT USAGE ON *.* TO 'cube'@'localhost';
-GRANT USAGE ON *.* TO 'cube-import'@'localhost';
+GRANT USAGE ON *.* TO 'toolkitimport'@'localhost';
 # Now drop the users
 DROP USER 'cube'@'localhost';
-DROP USER 'cube-import'@'localhost';
+DROP USER 'toolkitimport'@'localhost';
 
 # And re-create
 CREATE USER 'cube'@'localhost' IDENTIFIED BY 'hialpabg';
-CREATE USER 'cube-import'@'localhost' IDENTIFIED BY 'spanner';
+CREATE USER 'toolkitimport'@'localhost' IDENTIFIED BY 'spanner';
 
 DROP DATABASE IF EXISTS cube;
+DROP DATABASE IF EXISTS toolkitimport;
 CREATE DATABASE `cube` CHARACTER SET 'utf8';
+CREATE DATABASE `toolkitimport` CHARACTER SET 'utf8';
 
 # Give the general user permissions on the djanog db:
 GRANT ALTER,CREATE,DELETE,DROP,INDEX,INSERT,SELECT,SHOW VIEW,UPDATE ON `cube`.* to `cube`@`localhost`;
 
 # Give the import script user quite a lot of power...
-GRANT ALL PRIVILEGES ON `cube`.* TO 'cube-import'@'localhost';
-GRANT SELECT ON `mysql`.* TO 'cube-import'@'localhost';
-GRANT SELECT ON `toolkit`.* TO 'cube-import'@'localhost';
-
-# GRANT SELECT ON `cube-import`.* TO 'cube'@'localhost';
+GRANT ALL PRIVILEGES ON `toolkitimport`.* TO 'toolkitimport'@'localhost';
+GRANT SELECT ON `mysql`.* TO 'toolkitimport'@'localhost';
 
 FLUSH PRIVILEGES;
 
