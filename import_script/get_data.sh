89d84de31fd126f70296171a46d568d17d6cc390
Ben Motz <ben@ouat.co.uk>
2013-06-06 22:25:03 +0100

    Change get_data.sh to do a local copy when running on sparror

--- import_script/get_data.sh
+++ import_script/get_data.sh
@@ -12,6 +12,16 @@ MEDIA_PATH="../media"
 
 LOCAL_PATH="./source_data"
 
+if [ $(hostname -s) = sparror ] ; then
+echo "Running on sparror, doing local copy"
+SRC_ROOT=
+else
+SRC_ROOT=$USER@$SERVER:
+fi
+
+# Give up if a command fails
+set -e 
+
 ##############################################################################
 
 DIARY_SERVER_PATH="/home/cube/cgi-bin/diary/data"
@@ -45,27 +55,27 @@ VOL_PHOTO_THUMBS_SERVER_PATH="/home/cube/htdocs/volunteers/portraits/thumb/"
 VOL_PHOTO_THUMBS_LOCAL_PATH="$MEDIA_PATH/volunteers_thumbnails/"
 
 echo "Getting Diary"
-$RSYNC -av --exclude='.svn' $USER@$SERVER:$DIARY_SERVER_PATH/*.dat $DIARY_LOCAL_PATH/
+$RSYNC -av --exclude='.svn' ${SRC_ROOT}$DIARY_SERVER_PATH/*.dat $DIARY_LOCAL_PATH/
 echo "Getting Events"
-$RSYNC -av --exclude='.svn' $USER@$SERVER:$EVENTS_SERVER_PATH/*.dat $EVENTS_LOCAL_PATH/
+$RSYNC -av --exclude='.svn' ${SRC_ROOT}$EVENTS_SERVER_PATH/*.dat $EVENTS_LOCAL_PATH/
 echo "Getting Event roles"
-$RSYNC -av --exclude='.svn' $USER@$SERVER:$EVENTS_SERVER_PATH/roles/* $EVENTS_LOCAL_PATH/roles/
+$RSYNC -av --exclude='.svn' ${SRC_ROOT}$EVENTS_SERVER_PATH/roles/* $EVENTS_LOCAL_PATH/roles/
 
 echo "Getting Members"
-$RSYNC -av --exclude='.svn' $USER@$SERVER:$MEMBERS_SERVER_FILE $MEMBERS_LOCAL_PATH
+$RSYNC -av --exclude='.svn' ${SRC_ROOT}$MEMBERS_SERVER_FILE $MEMBERS_LOCAL_PATH
 echo "Getting Volunteer notes"
-$RSYNC -av --exclude='.svn' $USER@$SERVER:$VOLUNTEERS_SERVER_FILE $VOLUNTEERS_LOCAL_PATH
+$RSYNC -av --exclude='.svn' ${SRC_ROOT}$VOLUNTEERS_SERVER_FILE $VOLUNTEERS_LOCAL_PATH
 echo "Getting Volunteer roles"
-$RSYNC -av --exclude='.svn' $USER@$SERVER:$ROLES_SERVER_PATH/ $ROLES_LOCAL_PATH/
+$RSYNC -av --exclude='.svn' ${SRC_ROOT}$ROLES_SERVER_PATH/ $ROLES_LOCAL_PATH/
 echo "Getting Event formats"
-$RSYNC -av --exclude='.svn' $USER@$SERVER:$FORMATS_SERVER_PATH/ $FORMATS_LOCAL_PATH/
+$RSYNC -av --exclude='.svn' ${SRC_ROOT}$FORMATS_SERVER_PATH/ $FORMATS_LOCAL_PATH/
 echo "Getting Volunteer photos"
-$RSYNC -av --exclude='.svn' --exclude='thumb' --progress $USER@$SERVER:$VOL_PHOTO_SERVER_PATH $VOL_PHOTO_LOCAL_PATH
+$RSYNC -av --exclude='.svn' --exclude='thumb' --progress ${SRC_ROOT}$VOL_PHOTO_SERVER_PATH $VOL_PHOTO_LOCAL_PATH
 echo "Getting Event photos"
-$RSYNC -av --exclude='.svn' --exclude='thumb' --progress $USER@$SERVER:$EVENT_PHOTO_SERVER_PATH $EVENT_PHOTO_LOCAL_PATH
+$RSYNC -av --exclude='.svn' --exclude='thumb' --progress ${SRC_ROOT}$EVENT_PHOTO_SERVER_PATH $EVENT_PHOTO_LOCAL_PATH
 echo "Getting Volunteer thumbs"
-$RSYNC -av --exclude='.svn' --progress $USER@$SERVER:$VOL_PHOTO_THUMBS_SERVER_PATH $VOL_PHOTO_THUMBS_LOCAL_PATH
+$RSYNC -av --exclude='.svn' --progress ${SRC_ROOT}$VOL_PHOTO_THUMBS_SERVER_PATH $VOL_PHOTO_THUMBS_LOCAL_PATH
 echo "Getting Event thumbs"
-$RSYNC -av --exclude='.svn' --progress $USER@$SERVER:$EVENT_PHOTO_THUMBS_SERVER_PATH $EVENT_PHOTO_THUMBS_LOCAL_PATH
+$RSYNC -av --exclude='.svn' --progress ${SRC_ROOT}$EVENT_PHOTO_THUMBS_SERVER_PATH $EVENT_PHOTO_THUMBS_LOCAL_PATH
 
 exit 0
