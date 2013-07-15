74c7e1085e61e821ec0fcce81382f1ac2d596c23
Ben Motz <ben@ouat.co.uk>
2013-07-15 21:42:17 +0100

    Update bootstrap.sh to install write/develop/deploy requirements

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-# Trivial script to create virtualenv and do initial package install
+# Trivial script to create virtualenv and do initial package install for dev
+# ore deployment
 # (Requires system pip to be available)
 VENV_PATH=./venv
+# Requirements to run the django app:
 REQUIREMENTS_FILE=requirements_python_only.txt
+# Requirements to develop/test/deploy:
+REQUIREMENTS_DEV=requirements_development.txt
 
 set -e
 
 virtualenv $VENV_PATH --system-site-packages
 source $VENV_PATH/bin/activate
 pip install --requirement $REQUIREMENTS_FILE
+pip install --requirement $REQUIREMENTS_DEV
