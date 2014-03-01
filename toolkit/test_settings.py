deead6c3ec1b57fd2551e36166826fd3d713b24f
Ben Motz <ben@ouat.co.uk>
2014-03-01 21:01:52 +0000

    More-or-less tidy up console output when running tests

--- toolkit/test_settings.py
+++ toolkit/test_settings.py
@@ -14,9 +14,6 @@ DATABASES = {
 # Different to the key used in production!
 SECRET_KEY = '*@t04l3a7+uos5*7=c4ph1t#s(l*tlcdx(n(isztw^4w2c&mu-'
 
-LOGGING['handlers']['file']['filename'] = 'django_test.log'
-LOGGING['root'] = {}
-
 MEDIA_ROOT = '/tmp'
 
 PASSWORD_HASHERS = (
@@ -24,4 +21,19 @@ PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
 
-
+# Minimal logging configuration, that should eat almost everything
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+        # NullHandler would be better, but that requires python 2.7
+        'console': {
+            'level': 'CRITICAL',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'CRITICAL',
+    }
+}
