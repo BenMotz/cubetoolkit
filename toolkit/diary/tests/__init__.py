9e0b5824765fb8136fe5491b4aaf157340b2bbd4
Ben Motz <ben@ouat.co.uk>
2014-08-13 00:08:17 +0100

    Add some basic tests of the archive pages

--- toolkit/diary/tests/__init__.py
+++ toolkit/diary/tests/__init__.py
@@ -6,5 +6,6 @@ from .test_feeds import *
 from .test_mailout_view import *
 from .test_models import *
 from .test_public_views import *
+from .test_archive_views import *
 from .test_printedprogramme_views import *
 from .test_templatetags_showing_date_format import *
---
+++ toolkit/diary/tests/test_archive_views.py
@@ -0,0 +1,62 @@
+from __future__ import absolute_import
+
+from mock import patch
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from .common import DiaryTestsMixin
+
+class ArchiveViews(DiaryTestsMixin, TestCase):
+    def setUp(self):
+        super(ArchiveViews, self).setUp()
+
+        self.time_patch = patch('django.utils.timezone.now')
+        time_mock = self.time_patch.start()
+        time_mock.return_value = self._fake_now
+
+    def tearDown(self):
+        self.time_patch.stop()
+
+    def test_view_index(self):
+        url = reverse("archive-view-index")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "showing_archive.html")
+
+    def test_view_year(self):
+        url = reverse("archive-view-year", kwargs={"year": "2013"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "showing_archive_year.html")
+
+    def test_view_month(self):
+        url = reverse("archive-view-month", kwargs={"year": "2013", "month": "4"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "showing_archive_month.html")
+
+    def test_view_search(self):
+        url = reverse("archive-search")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "showing_archive_search.html")
+
+    def test_view_search_post_not_allowed(self):
+        url = reverse("archive-search")
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 405)
+
+    def test_run_search_no_content(self):
+        url = reverse("archive-search")
+        response = self.client.get(url, data={"search_term": ""})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "showing_archive_search.html")
+        self.assertContains(response, "0 Results")
+
+    def test_run_search(self):
+        url = reverse("archive-search")
+        response = self.client.get(url, data={"search_term": "event"})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "showing_archive_search.html")
+        self.assertContains(response, "4 Results")
