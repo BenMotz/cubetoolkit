5a1ca3501bd70d8098d8d899c10886f440a28af4
Ben Motz <ben@ouat.co.uk>
2015-04-09 20:16:23 +0100

    Improve the auto tag script
    With a view to possibly using it again sometime

--- import_script/auto_tag.py
+++ import_script/auto_tag.py
@@ -1,34 +1,20 @@
 #!/usr/bin/env python
-import logging
-
 from toolkit.diary.models import Event, EventTag
-from django.db.models import Q
 
 
-logger = logging.getLogger('toolkit.import')
+def add_tag(event, tag):
+    if not tag.event_set.filter(pk=event.pk):
+        print "  %s" % event
+        tag.event_set.add(event)
 
-def tag_events_with_name_containing(text, tags):
-    events = Event.objects.filter(name__icontains=text)
+
+def tag_events_with_field_containing(field, text, tags):
+    filter_var = {("%s__icontains" % field): text}
+    events = Event.objects.filter(**filter_var)
     for tag in tags:
         tag = EventTag.objects.get(name=tag)
         for e in events:
-            tag.event_set.add(e)
-
-
-def tag_events_with_copy_containing(text, tags):
-    events = Event.objects.filter(copy__icontains=text)
-    for tag in tags:
-        tag = EventTag.objects.get(name=tag)
-        for e in events:
-            tag.event_set.add(e)
-
-
-def tag_events_with_terms_containing(text, tags):
-    events = Event.objects.filter(terms__icontains=text)
-    for tag in tags:
-        tag = EventTag.objects.get(name=tag)
-        for e in events:
-            tag.event_set.add(e)
+            add_tag(e, tag)
 
 
 def tag_ttt_as_film():
@@ -36,56 +22,32 @@ def tag_ttt_as_film():
     ttt = Event.objects.filter(showings__discounted=True)
     tag = EventTag.objects.get(name="film")
     for e in ttt:
-        tag.event_set.add(e)
-    # Belt and braces:
-    tag_events_with_copy_containing("TTT", ("film", "35mm"))
-
-
-def tag_certs_as_film():
-    # Assume all discounted things are TTT and are film
-    logger.info("Running certificate based film tag guessing...")
-    certificates = ('PG', 'U', '15', '12', '12A', '18')
-    cert_text = []
-    cert_text.extend("cert " + c for c in certificates)
-    cert_text.extend("cert" + c for c in certificates)
-    cert_text.extend("cert." + c for c in certificates)
-    cert_text.extend("cert. " + c for c in certificates)
-    cert_text.extend("cert:" + c for c in certificates)
-    cert_text.extend("cert: " + c for c in certificates)
-    cert_text.extend("certificate " + c for c in certificates)
-    cert_text.extend("certificate: " + c for c in certificates)
-    query = Q()
-    for ct in cert_text:
-        query = query | Q(copy__icontains=ct)
-
-    events = Event.objects.filter(query)
-    tag = EventTag.objects.get(name="film")
-    logger.info("Certificates lead to %d events being tagged as films" % len(query))
-    for e in events:
-        tag.event_set.add(e)
-    # Belt and braces:
-    tag_events_with_copy_containing("TTT", ("film", "35mm"))
+        add_tag(e, tag)
 
 
 def main():
     copy_map = {
-        "nanoplex": ("nanoplex",),
+        "nanoplex": ("nanoplex", ),
         "babycinema": ("babycinema", "film"),
         "cabaret": ("cabaret", "party"),
-        "hkkp": ("hkkp",),
         "rescore": ("film", "music"),
-        "documentary": ("film",),
-        "indymedia": ('indymedia',),
-        "orchestra": ('cubeorchestra',),
-        "bluescreen": ('bluescreen',),
+        "documentary": ("film", ),
+        "indymedia": ('indymedia', ),
+        "orchestra cube": ('cubeorchestra', ),
+        "cube orchestra": ('cubeorchestra', ),
+        "cubeorchestra": ('cubeorchestra', ),
+        "bluescreen": ('bluescreen', ),
+        "party": ('party', ),
+        "outdoor": ('outdoors', ),
+        "q&a": ('q&a', ),
     }
     for text, tags in copy_map.iteritems():
-        logger.info("Tagging all events containing the text '%s' with tags '%s'" % (text, ", ".join(tags)))
-        tag_events_with_terms_containing(text, tags)
-        tag_events_with_name_containing(text, tags)
+        print "Tagging any events containing the text '%s' with tags '%s':" % (
+            text, ", ".join(tags))
+        for field in ('terms', 'name'):
+            tag_events_with_field_containing(field, text, tags)
 
     tag_ttt_as_film()
-    tag_certs_as_film()
 
 
 if __name__ == "__main__":
