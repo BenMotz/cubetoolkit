62b01f72bddcf25f6646adfb65dadbe2b2622eb0
Ben Motz <ben@ouat.co.uk>
2017-09-30 22:42:40 +0100

    Consolidate migrations

--- toolkit/content/migrations/0001_initial.py
+++ toolkit/content/migrations/0001_initial.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 import wagtail.wagtailcore.fields
 import django.db.models.deletion
+import modelcluster.fields
 
 
 class Migration(migrations.Migration):
@@ -19,11 +20,42 @@ class Migration(migrations.Migration):
             fields=[
                 ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
                 ('body', wagtail.wagtailcore.fields.RichTextField()),
+                ('image_alignment', models.CharField(default=b'L', max_length=3, choices=[(b'L', b'Left'), (b'R', b'Right')])),
                 ('image', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
             ],
             options={
                 'abstract': False,
             },
             bases=('wagtailcore.page',),
+        ),
+        migrations.CreateModel(
+            name='ImageGalleryImage',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('sort_order', models.IntegerField(null=True, editable=False, blank=True)),
+                ('caption', models.CharField(max_length=255, blank=True)),
+                ('image', models.ForeignKey(related_name='+', to='wagtailimages.Image')),
+            ],
+            options={
+                'ordering': ['sort_order'],
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='ImageGalleryPage',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                ('intro_text', wagtail.wagtailcore.fields.RichTextField(blank=True)),
+                ('footer_text', wagtail.wagtailcore.fields.RichTextField(blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.AddField(
+            model_name='imagegalleryimage',
+            name='page',
+            field=modelcluster.fields.ParentalKey(related_name='gallery_images', to='content.ImageGalleryPage'),
         ),
     ]
