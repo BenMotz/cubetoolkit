3be51253debcadc7fccefd89e9787b66c7f34664
Ben Motz <ben@ouat.co.uk>
2017-09-30 23:34:35 +0100

    Add link listing page

--- toolkit/content/models.py
+++ toolkit/content/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.http import Http404
 
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Page, Orderable
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from modelcluster.fields import ParentalKey
 
@@ -54,4 +55,34 @@ class ImageGalleryImage(Orderable):
     panels = [
         ImageChooserPanel('image'),
         FieldPanel('caption'),
+    ]
+
+
+class SectionRootWithLinks(Page):
+    content_panels = Page.content_panels + [
+        InlinePanel('links', label='Links'),
+    ]
+
+    # Hide 'slug', 'SEO title' etc, as they're not relevant:
+    promote_panels = [
+        MultiFieldPanel([
+            FieldPanel('show_in_menus'),
+        ], 'Common page configuration')
+    ]
+    settings_panels = None
+
+    def serve(self, request):
+        # For now, don't actually serve anything for this page:
+        raise Http404("Page not found")
+
+
+class SectionLink(Orderable):
+    page = ParentalKey(SectionRootWithLinks, related_name='links')
+
+    text = models.CharField(max_length=1024, blank=True, null=False)
+    link = models.URLField(max_length=1024, blank=False, null=False)
+
+    panels = [
+        FieldPanel('link'),
+        FieldPanel('text'),
     ]
