490dd32bfd1f7fe424ec4e32611a8647ffed4d3d
Ben Motz <ben@ouat.co.uk>
2017-10-07 22:37:47 +0100

    Fixup constant compare time for py3 compat

--- toolkit/util/__init__.py
+++ toolkit/util/__init__.py
@@ -1,18 +1,20 @@
 import random
 import string
 
+import six
+
 
 def compare_constant_time(a, b):
     """Compare a and b in constant time.
     Use to reduce risk of timing attacks"""
-    assert isinstance(a, str)
-    assert isinstance(b, str)
+    assert isinstance(a, six.binary_type)
+    assert isinstance(b, six.binary_type)
     if len(a) != len(b):
         return False
 
     result = 0
-    for x, y in zip(a, b):
-        result |= ord(x) ^ ord(y)
+    for x, y in zip(bytearray(a), bytearray(b)):
+        result |= x ^ y
     return result == 0
 
 
