c6e929990db952d202a95bdd3222a11810bf1ec0
Ben Motz <ben@ouat.co.uk>
2017-10-09 21:01:41 +0100

    Add some missing test cases

--- toolkit/index/tests.py
+++ toolkit/index/tests.py
@@ -38,8 +38,8 @@ class TestViews(TestCase):
     # Fairly incomplete set of tests, but good enough
 
     def setUp(self):
-        cat1 = IndexCategory(name="Category 1 Links!")
-        cat1.save()
+        self.cat1 = IndexCategory(name="Category 1 Links!")
+        self.cat1.save()
 
         cat2 = IndexCategory(name="Category 2 Links!")
         cat2.save()
@@ -48,7 +48,7 @@ class TestViews(TestCase):
         cat3.save()
 
         l1 = IndexLink(text="Link one", link="http://link1.test.com/")
-        l1.category = cat1
+        l1.category = self.cat1
         l1.save()
 
         l2 = IndexLink(text="Two Link", link="http://cubecinema.com/")
@@ -90,6 +90,27 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('toolkit_index.html')
 
+    def test_create_link(self):
+        link_name = 'Superior quality link'
+        link_url = 'http://i.like.an.example.com'
+        url = reverse("create-index-link")
+        response = self.client.get(url)
+        # check form loads
+        self.assertEqual(response.status_code, 200)
+
+        # check create works:
+        response = self.client.post(url, data={
+            'text': link_name,
+            'link': link_url,
+            'category': 1,
+        })
+        self.assertRedirects(response, reverse("toolkit-index"))
+
+        link = IndexLink.objects.get(id=4)
+        self.assertEqual(link.text, link_name)
+        self.assertEqual(link.link, link_url)
+        self.assertEqual(link.category, self.cat1)
+
     def test_edit_link(self):
         url = reverse("update-index-link", kwargs={"pk": "1"})
         response = self.client.post(url, data={
@@ -103,6 +124,23 @@ class TestViews(TestCase):
         self.assertEqual(link.text, 'All new link text!')
         self.assertEqual(link.link, 'http://boring.com/')
         self.assertEqual(link.category_id, 2)
+
+    def test_create_category(self):
+        category_name = 'My new category of fish'
+        # url = reverse("create-index-link", kwargs={"pk": "1"})
+        url = reverse("create-index-category")
+        response = self.client.get(url)
+        # check form loads
+        self.assertEqual(response.status_code, 200)
+
+        # check create works:
+        response = self.client.post(url, data={
+            'name': category_name,
+        })
+        self.assertRedirects(response, reverse("toolkit-index"))
+
+        category = IndexCategory.objects.get(id=4)
+        self.assertEqual(category.name, category_name)
 
     def test_edit_category(self):
         url = reverse("update-index-category", kwargs={"pk": "1"})
