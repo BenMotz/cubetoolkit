98985f0807780f9da5b393f7106b4a73bbb41191
Ben Motz <ben@ouat.co.uk>
2017-10-11 22:43:12 +0100

    Refactor member tests a bit

---
+++ toolkit/members/tests/common.py
@@ -0,0 +1,122 @@
+from django.conf import settings
+import django.contrib.contenttypes as contenttypes
+import django.contrib.auth.models as auth_models
+
+from toolkit.members.models import Member, Volunteer
+from toolkit.diary.models import Role
+
+class MembersTestsMixin(object):
+
+    def setUp(self):
+        self._setup_test_data()
+        self._setup_test_users()
+
+        return super(MembersTestsMixin, self).setUp()
+
+    def _setup_test_data(self):
+        # Roles:
+        r1 = Role(name="Role 1 (standard)", read_only=False, standard=True)
+        r1.save()
+        r2 = Role(name="Role 2 (nonstandard)", read_only=False, standard=False)
+        r2.save()
+        r3 = Role(name="Role 3", read_only=False, standard=False)
+        r3.save()
+
+        # Members:
+        self.mem_1 = Member(name=u"Member On\u0205",
+                            email="one@example.com", number="1",
+                            postcode="BS1 1AA")
+        self.mem_1.save()
+        self.mem_2 = Member(name=u"Tw\u020d Member",
+                            email="two@example.com", number="02",
+                            postcode="")
+        self.mem_2.save()
+        self.mem_3 = Member(name=u"Some Third Chap",
+                            email="two@member.test", number="000",
+                            postcode="NORAD")
+        self.mem_3.save()
+        self.mem_4 = Member(name="Volunteer One", email="volon@cube.test",
+                            number="3", phone="0800 000 000", address="1 Road",
+                            posttown="Town of towns", postcode="BS6 123",
+                            country="UKountry", website="http://1.foo.test/")
+        self.mem_4.save()
+        self.mem_5 = Member(name="Volunteer Two", email="", number="4",
+                            phone="", altphone="", address="", posttown="",
+                            postcode="", country="",
+                            website="http://two.foo.test/")
+        self.mem_5.save()
+        self.mem_6 = Member(name="Volunteer Three", email="volthree@foo.test",
+                            number="4", phone="", altphone="", address="",
+                            posttown="", postcode="", country="", website="")
+        self.mem_6.save()
+        self.mem_7 = Member(name="Volunteer Four", email="four4@foo.test",
+                            number="o4", phone="", altphone="", address="",
+                            posttown="", postcode="", country="", website="")
+        self.mem_7.save()
+        self.mem_8 = Member(name=u"Number Eight, No mailout please",
+                            email="bart@bart.test", number="010",
+                            mailout=False)
+        self.mem_8.save()
+        self.mem_8 = Member(name=u"Number Nine, mailout failed",
+                            email="frobney@squoo.test", number="010",
+                            mailout_failed=True)
+        self.mem_8.save()
+
+        # Volunteers:
+        self.vol_1 = Volunteer(
+            member=self.mem_4, notes=u'Likes the $, the \u00a3 and the \u20ac',
+            portrait=settings.MEDIA_ROOT + "/path/to/portrait"
+        )
+        self.vol_1.save()
+        self.vol_1.roles = [r1, r3]
+        self.vol_1.save()
+
+        self.vol_2 = Volunteer(member=self.mem_5)
+        self.vol_2.save()
+
+        self.vol_3 = Volunteer(member=self.mem_6)
+        self.vol_3.save()
+        self.vol_3.roles = [r3]
+        self.vol_3.save()
+
+        self.vol_4 = Volunteer(
+            member=self.mem_7, active=False,
+            notes=u"Subliminal, superluminous")
+        self.vol_4.save()
+        self.vol_4.roles = [r3]
+        self.vol_4.save()
+
+    def _setup_test_users(self):
+        # Read/write user:
+        user_rw = auth_models.User.objects.create_user(
+            'admin', 'toolkit_admin@localhost', 'T3stPassword!')
+        # read only user:
+        user_r = auth_models.User.objects.create_user(
+            'read_only', 'toolkit_admin@localhost', 'T3stPassword!1')
+        # no permission user:
+        auth_models.User.objects.create_user(
+            'no_perm', 'toolkit_admin@localhost', 'T3stPassword!2')
+        # Create dummy ContentType:
+        ct = contenttypes.models.ContentType.objects.get_or_create(
+            model='',
+            app_label='toolkit'
+        )[0]
+        # Create 'read' and 'write' permissions:
+        write_permission = auth_models.Permission.objects.get_or_create(
+            name='Write access to all toolkit content',
+            content_type=ct,
+            codename='write'
+        )[0]
+
+        read_permission = auth_models.Permission.objects.get_or_create(
+            name='Read access to all toolkit content',
+            content_type=ct,
+            codename='read'
+        )[0]
+        # Set user permissions, r/w:
+        user_rw.user_permissions.add(write_permission)
+        user_rw.user_permissions.add(read_permission)
+        # read only:
+        user_r.user_permissions.add(read_permission)
+
+
