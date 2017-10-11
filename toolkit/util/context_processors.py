fed4bc43ba28eea90e445b161f2137c6f7230321
Marcus Valentine <marcus@marcusv.org>
2017-10-11 12:19:56 +0100

    Refactor by passing VENUE as a dict in toolkit/util/context_processors.py

--- toolkit/util/context_processors.py
+++ toolkit/util/context_processors.py
@@ -1,9 +1,5 @@
 from django.conf import settings
 
 
-def venue_longname(request):
-    return {'VENUE_LONGNAME': settings.VENUE['longname']}
-
-
-def venue_int_header_img(request):
-    return {'VENUE_HEADER_IMG': settings.VENUE['internal_header_img']}
+def venue(request):
+    return {'VENUE': settings.VENUE}
