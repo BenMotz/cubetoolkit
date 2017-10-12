351e88dd95db81418cc6d2deb4a943e2659292bc
Ben Motz <ben@ouat.co.uk>
2017-10-12 20:03:46 +0100

    Remove virtualenv setup from WSGI entrypoint
    Handle it in front end server instead.

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,4 @@
 import os
-import sys
-import site
-
-VIRTUALENV="venv"
-
-# Get site root from this file's location:
-SITE_ROOT=os.path.abspath(os.path.dirname(__file__))
-
-# Add virtualenv path to site package root:
-site.addsitedir(os.path.join(SITE_ROOT, VIRTUALENV, "lib/python2.7/site-packages"))
-site.addsitedir(os.path.join(SITE_ROOT, VIRTUALENV, "lib/python2.6/site-packages"))
-
-# Add site package root to start of pythonpath:
-sys.path.insert(0, SITE_ROOT)
 
 # celery should now be available (on the virtualenv path)
 import djcelery
