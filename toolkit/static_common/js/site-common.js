2a491b8fe9a625aff85e980c50b9dd56d1e31fc9
Ben Motz <ben@cubecinema.com>
2017-10-23 22:55:39 +0100

    Restore correct masonry config for index page
    When consolidating the JS, I hadn't noticed that the masonry setup was
    different for the index page. Whoops. Restore different configuration.

--- toolkit/static_common/js/site-common.js
+++ toolkit/static_common/js/site-common.js
@@ -1,85 +1,90 @@
 /* Common setup for public content pages */
-
-$(document).ready(function($){
-    /* init Masonry -------------------------------------------------------- */
-    var $grid = $('.programme').masonry({
-        percentPosition: true,
-        gutter: '.gutter-sizer',
-        columnWidth: '.grid-sizer',
-        itemSelector: '.grid-item'
-    });
-
-    // re-layout Masonry after each image loads
-    $grid.imagesLoaded().progress(function() {
-       $grid.masonry('layout');
-    });
-
-    /* set up small form factor menu --------------------------------------- */
-    $("#mobile-menu-btn").click(function() {
-        var toggle_el = $(this).data("toggle");
-        $(toggle_el).toggleClass("open-sidebar");
-        $(".black_overlay").toggleClass("active-search-bg");
-    });
-
-    $(".black_overlay").swipe({
-        swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-            $(".grid").removeClass("open-sidebar");
-            $(".black_overlay").removeClass("active-search-bg");
-        },
-        threshold: 0
-    });
-
-    /* Set up nav menu ----------------------------------------------------- */
-    $('#navmenu > ul > li:has(ul)').addClass("has-sub");
-
-    $('#navmenu > ul > li > a').click(function() {
-        var checkElement = $(this).next();
-
-        $('#navmenu li').removeClass('active');
-        $(this).closest('li').addClass('active');
-
-        if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
-            $(this).closest('li').removeClass('active');
-            checkElement.slideUp('normal');
+function setup_page(index_page) {
+    $(document).ready(function($){
+        var masonry_opts_index = {
+            gutter: '.gutter-sizer',
+            columnWidth: '.showing-sizer',
+            itemSelector: '.showing'
+        };
+        var masonry_opts_other = {
+            percentPosition: true,
+            gutter: '.gutter-sizer',
+            columnWidth: '.grid-sizer',
+            itemSelector: '.grid-item'
         }
+        var masonry_opts = index_page ? masonry_opts_index : masonry_opts_other;
 
-        if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
-            $('#navmenu ul ul:visible').slideUp('normal');
-            checkElement.slideDown('normal');
-        }
+        /* init Masonry -------------------------------------------------------- */
+        var $grid = $('.programme').masonry(masonry_opts);
 
-        if (checkElement.is('ul')) {
-            return false;
-        } else {
-            return true;
-        }
-    });
-
-    /* Set up grid/list switcher (event list only) ------------------------- */
-    $('a#listbtn.switcher').click(function() {
-        $('.programme').animate({opacity: 0},function() {
-            $('.programme').hide();
-            $('.list').addClass('active');
-            $('.list.active').stop().animate({opacity: 1});
+        // re-layout Masonry after each image loads
+        $grid.imagesLoaded().progress(function() {
+           $grid.masonry('layout');
         });
-    });
 
-    $('a#gridbtn.switcher').click(function() {
-        $('.list').animate({opacity:0},function() {
-            $('.list').removeClass('active');
-            $('.programme').show();
-            $('.programme').masonry({
-                gutter: '.gutter-sizer',
-                columnWidth: '.showing-sizer',
-                itemSelector: '.showing'
+        /* set up small form factor menu --------------------------------------- */
+        $("#mobile-menu-btn").click(function() {
+            var toggle_el = $(this).data("toggle");
+            $(toggle_el).toggleClass("open-sidebar");
+            $(".black_overlay").toggleClass("active-search-bg");
+        });
+
+        $(".black_overlay").swipe({
+            swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+                $(".grid").removeClass("open-sidebar");
+                $(".black_overlay").removeClass("active-search-bg");
+            },
+            threshold: 0
+        });
+
+        /* Set up nav menu ----------------------------------------------------- */
+        $('#navmenu > ul > li:has(ul)').addClass("has-sub");
+
+        $('#navmenu > ul > li > a').click(function() {
+            var checkElement = $(this).next();
+
+            $('#navmenu li').removeClass('active');
+            $(this).closest('li').addClass('active');
+
+            if((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+                $(this).closest('li').removeClass('active');
+                checkElement.slideUp('normal');
+            }
+
+            if((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+                $('#navmenu ul ul:visible').slideUp('normal');
+                checkElement.slideDown('normal');
+            }
+
+            if (checkElement.is('ul')) {
+                return false;
+            } else {
+                return true;
+            }
+        });
+
+        /* Set up grid/list switcher (event list only) ------------------------- */
+        $('a#listbtn.switcher').click(function() {
+            $('.programme').animate({opacity: 0},function() {
+                $('.programme').hide();
+                $('.list').addClass('active');
+                $('.list.active').stop().animate({opacity: 1});
             });
-            $('.programme').stop().animate({opacity: 1});
+        });
+
+        $('a#gridbtn.switcher').click(function() {
+            $('.list').animate({opacity:0},function() {
+                $('.list').removeClass('active');
+                $('.programme').show();
+                $('.programme').masonry(masonry_opts);
+                $('.programme').stop().animate({opacity: 1});
+            });
         });
     });
-});
 
-// Belt and braces: do a final masonry layout after all images are
-// definitely loaded:
-$(window).load(function() {
-    $('.programme').masonry('layout');
-});
+    // Belt and braces: do a final masonry layout after all images are
+    // definitely loaded:
+    $(window).load(function() {
+        $('.programme').masonry('layout');
+    });
+}
