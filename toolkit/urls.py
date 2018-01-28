a33eea0cba60f9d156869e7313234d80a1cedf64
Marcus Valentine <marcus@marcusv.org>
2018-01-28 22:21:39 +0000

    Add id short code URL; add bootstrap CSS to HTML mailout

--- toolkit/urls.py
+++ toolkit/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     url(r'^auth/', include(toolkit.toolkit_auth.urls.urlpatterns)),
     url(r'^index/', include(toolkit.index.urls.urlpatterns)),
     url(r'^$', toolkit.diary.urls.view_diary, name="default-view"),
+    url(r'^id/(?P<event_id>\d+)/$', toolkit.diary.urls.view_event,
+        name="single-event-view"),
 
     # Main index page: requires logging in, even though some other parts
     # (eg diary index) don't.
