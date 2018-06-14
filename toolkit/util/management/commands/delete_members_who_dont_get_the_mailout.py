3018e79ab2c266674dfc74d1aaa49799b1ab98b7
Marcus Valentine <marcus@marcusv.org>
2018-06-14 12:25:17 +0100

    Add utils show_active_vols, show_email_duplicates, delete_members_who_dont_get_the_mailout

---
+++ toolkit/util/management/commands/delete_members_who_dont_get_the_mailout.py
@@ -0,0 +1,68 @@
+# https://docs.djangoproject.com/en/1.11/howto/custom-management-commands/
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from toolkit.members.models import Member
+
+
+def boolean_input(question, default=None):
+    result = input("%s " % question)
+    if not result and default is not None:
+        return default
+    while len(result) < 1 or result[0].lower() not in "yn":
+        result = input("Please answer yes or no: ")
+    return result[0].lower() == "y"
+
+
+class Command(BaseCommand):
+    help = "Delete members who don't get the members mailout"
+
+    def handle(self, *args, **options):
+
+        if not boolean_input("About to delete all members who don't get the mailout. Sure?"):
+            return
+
+        dead_wood = Member.objects.exclude(mailout=True)
+        vols_found = 0
+        active_vols_found = 0
+        verbose = False
+
+        self.stdout.write('Deleting...')
+
+        for member in dead_wood:
+            try:
+                member.volunteer
+                self.stdout.write(self.style.WARNING('Not deleting volunteer %s <%s> joined %s' % (
+                    member.name,
+                    member.email,
+                    member.created_at)
+                ))
+                vols_found = vols_found + 1
+
+                if member.volunteer.active:
+                    self.stdout.write(self.style.ERROR('Not deleting active volunteer %s <%s> joined %s' % (
+                        member.name,
+                        member.email,
+                        member.created_at)
+                    ))
+                    active_vols_found = active_vols_found + 1
+
+            except ObjectDoesNotExist:   # Not a volunteer
+                if verbose:
+                    self.stdout.write('%s <%s> joined %s, updated %s' % (
+                        member.name,
+                        member.email,
+                        member.created_at,
+                        member.updated_at)
+                    )
+                member.delete()
+
+        self.stdout.write(self.style.SUCCESS(
+            '\nDeleted %d members\n' % len(dead_wood)))
+
+        self.stdout.write(self.style.WARNING(
+            'Not deleted %d volunteers\n' % vols_found))
+
+        self.stdout.write(self.style.ERROR(
+            'Not deleted %d active volunteers\n' % active_vols_found))
---
+++ toolkit/util/management/commands/show_active_vols.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from toolkit.members.models import Volunteer
+
+
+class Command(BaseCommand):
+    help = "Show active volunteers"
+
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument(
+            '--names',
+            action='store_true',
+            dest='names',
+            default=False,
+            help='Show names as well as emails',
+        )
+
+    def handle(self, *args, **options):
+
+        vols = (Volunteer.objects.filter(active=True)
+                                 .order_by('member__name'))
+
+        for vol in vols:
+            if options['names']:
+                self.stdout.write('%s <%s>' %
+                                  (vol.member.name,
+                                   vol.member.email))
+            else:
+                self.stdout.write('%s' %
+                                  vol.member.email)
+
+        self.stdout.write(self.style.SUCCESS(
+            '\n%d active members' % len(vols)))
