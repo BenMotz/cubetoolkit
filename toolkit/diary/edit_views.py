2d0e32125568a89b953cbbf086b9b827c43e1e96
Marcus Valentine <marcus@marcusv.org>
2021-11-21 07:04:16 +0000

    On rota vacancies, change number of days from 6 to 30
    TODO: fix the test at tests/test_edit_rota.py, line 339

--- toolkit/diary/edit_views.py
+++ toolkit/diary/edit_views.py
@@ -929,7 +929,7 @@ def printed_programme_edit(request, operation):
 
 @permission_required('diary.change_rotaentry')
 def view_rota_vacancies(request):
-    days_ahead = 6
+    days_ahead = 30
     start = timezone.now()
     end_date = start + datetime.timedelta(days=days_ahead)
     showings = (Showing.objects.not_cancelled()
