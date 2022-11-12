a9d25c675147f8de88a329edd0c0fd57ae56c229
Ben Motz <ben@ouat.co.uk>
2022-11-12 22:03:46 +0000

    Allow startup if redis is not configured

--- containerconfig/tk_run.sh
+++ containerconfig/tk_run.sh
@@ -23,7 +23,7 @@ elif ! [[ -S /var/run/mysqld/mysqld.sock ]] ; then
   exit 3
 fi
 
-if ! wait-for-it $redis_host_port --timeout=360 ; then
+if [[ -n $redis_host_port ]] && ! wait-for-it $redis_host_port --timeout=360 ; then
   echo "Redis not available"
   exit 4
 fi
