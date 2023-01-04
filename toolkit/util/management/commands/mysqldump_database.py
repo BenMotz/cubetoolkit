e52cac848df5ff443a6d5485e6b25035a17d7cea
Ben Motz <ben@ouat.co.uk>
2023-01-04 21:12:27 +0000

    Enable output of mysqldump invocation

--- toolkit/util/management/commands/mysqldump_database.py
+++ toolkit/util/management/commands/mysqldump_database.py
@@ -21,6 +21,22 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("output_file", type=str)
 
+    def _dump_command(self):
+        db_settings = settings.DATABASES["default"]
+        if db_settings["ENGINE"] != "django.db.backends.mysql":
+            raise CommandError(
+                "Application does not use a mysql database (very sensible) - "
+                "it uses {0}".format(db_settings["engine"])
+            )
+
+        return [
+            MYSQLDUMP_BINARY,
+            "--single-transaction",  # So LOCK TABLES isn't needed
+            "--user=" + db_settings["USER"],
+            "--password=" + db_settings["PASSWORD"],
+            db_settings["NAME"],
+        ]
+
     def _dump_to_file(self, out_file, log_file):
         if not os.path.isfile(MYSQLDUMP_BINARY):
             raise CommandError(
@@ -29,32 +45,29 @@ class Command(BaseCommand):
                 )
             )
 
-        db_settings = settings.DATABASES["default"]
-        if db_settings["ENGINE"] != "django.db.backends.mysql":
-            raise CommandError(
-                "Application does not use a mysql database (very sensible) - "
-                "it uses {0}".format(db_settings["engine"])
-            )
-
         print("Dumping to '{0}'".format(out_file.name), file=log_file)
-        returncode = subprocess.call(
-            [
-                MYSQLDUMP_BINARY,
-                "--single-transaction",  # So LOCK TABLES isn't needed
-                "--user=" + db_settings["USER"],
-                "--password=" + db_settings["PASSWORD"],
-                db_settings["NAME"],
-            ],
-            stdout=out_file,
-        )
+        returncode = subprocess.call(self._dump_command(), stdout=out_file)
 
         if returncode != 0:
             raise CommandError("Failed (code {0})".format(returncode))
 
         print("Done", file=log_file)
 
+    def create_parser(self, prog_name, subcommand, **kwargs):
+        parser = super().create_parser(prog_name, subcommand, **kwargs)
+        parser.add_argument(
+            "--print-command",
+            action="store_true",
+            help="Print the mysqldump command, but do not run it",
+        )
+        return parser
+
     def handle(self, *args, **options):
         output_filename = options.get("output_file")
+
+        if options.get("print_command"):
+            self.stdout.write(" ".join(self._dump_command()))
+            return
 
         if not output_filename or not output_filename.strip():
             raise CommandError("No filename supplied")
