93d58b1647577f16301596eb64c804e4de20644c
Ben Motz <ben@ouat.co.uk>
2023-01-04 20:47:01 +0000

    Support dumping database to stdout

--- toolkit/util/management/commands/mysqldump_database.py
+++ toolkit/util/management/commands/mysqldump_database.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import sys
 import os.path
 import subprocess
 
@@ -9,7 +10,10 @@ MYSQLDUMP_BINARY = "/usr/bin/mysqldump"
 
 
 class Command(BaseCommand):
-    help = "Dump database to <output_file> using mysqldump"
+    help = (
+        "Dump database to <output_file> using mysqldump. "
+        "Dumps to stdout if <output_file> is 'STDOUT'"
+    )
 
     can_import_settings = True
     requires_system_checks = True
@@ -17,12 +21,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("output_file", type=str)
 
-    def _dump_to_filename(self, filename):
-        if os.path.exists(filename):
-            raise CommandError(
-                "Dump destination '{0}' already exists".format(filename)
-            )
-
+    def _dump_to_file(self, out_file, log_file):
         if not os.path.isfile(MYSQLDUMP_BINARY):
             raise CommandError(
                 "Couldn't find mysqldump binary (looked for {0}".format(
@@ -37,23 +36,22 @@ class Command(BaseCommand):
                 "it uses {0}".format(db_settings["engine"])
             )
 
-        with open(filename, "w") as out_file:
-            print("Dumping to '{0}'".format(filename))
-            returncode = subprocess.call(
-                [
-                    MYSQLDUMP_BINARY,
-                    "--single-transaction",  # So LOCK TABLES isn't needed
-                    "--user=" + db_settings["USER"],
-                    "--password=" + db_settings["PASSWORD"],
-                    db_settings["NAME"],
-                ],
-                stdout=out_file,
-            )
+        print("Dumping to '{0}'".format(out_file.name), file=log_file)
+        returncode = subprocess.call(
+            [
+                MYSQLDUMP_BINARY,
+                "--single-transaction",  # So LOCK TABLES isn't needed
+                "--user=" + db_settings["USER"],
+                "--password=" + db_settings["PASSWORD"],
+                db_settings["NAME"],
+            ],
+            stdout=out_file,
+        )
 
         if returncode != 0:
             raise CommandError("Failed (code {0})".format(returncode))
 
-        print("Done")
+        print("Done", file=log_file)
 
     def handle(self, *args, **options):
         output_filename = options.get("output_file")
@@ -61,4 +59,12 @@ class Command(BaseCommand):
         if not output_filename or not output_filename.strip():
             raise CommandError("No filename supplied")
 
-        self._dump_to_filename(output_filename)
+        if output_filename == "STDOUT":
+            self._dump_to_file(sys.stdout, log_file=sys.stderr)
+        elif os.path.exists(output_filename):
+            raise CommandError(
+                "Dump destination '{0}' already exists".format(output_filename)
+            )
+        else:
+            with open(output_filename, "w") as out_file:
+                self._dump_to_file(out_file, log_file=sys.stdout)
