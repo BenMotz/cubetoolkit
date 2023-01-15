fb2dd847646104ac367331d6a05e8137ceebd138
Ben Motz <ben@ouat.co.uk>
2023-01-15 23:00:19 +0000

    Remove unused file

--- toolkit/deploy_settings.py
+++
@@ -1,6 +0,0 @@
-from toolkit.settings import *
-
-# This settings file is used by the deploy script for running various manage.py
-# commands: it's needed because the user which the deploy script runs as
-# doesn't necessarily have write permissions to the usual system logfile
-LOGGING["handlers"]["file"]["filename"] = os.path.join(BASE_DIR, "deploy.log")
