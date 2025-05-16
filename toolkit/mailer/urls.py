782debed7291c530a25e36464293537fb2cb53da
Ben Motz <ben@ouat.co.uk>
2025-05-16 23:21:12 +0100

    Use htmx for jobs list interactivity

--- toolkit/mailer/urls.py
+++ toolkit/mailer/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import job_create, job_cancel, jobs_list
+from .views import job_create, job_cancel, jobs_list, jobs_table
 
 app_name = "mailer"
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path("job/<int:job_id>/delete/", job_cancel, name="job-delete"),
     # View all jobs
     path("jobs/", jobs_list, name="jobs-list"),
+    # Retrieve job table fragment
+    path("job-table/", jobs_table, name="jobs-table"),
 ]
--- toolkit/mailer/views.py
+++ toolkit/mailer/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import permission_required
 from django.urls import reverse
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
 from django.shortcuts import get_object_or_404, render
 
 from .forms import JobForm
@@ -28,11 +28,7 @@ def job_cancel(request: HttpRequest, job_id: int) -> HttpResponse:
     return HttpResponseRedirect(reverse("mailer:jobs-list"))
 
 
-@permission_required("toolkit.read")
-def jobs_list(request) -> HttpResponse:
-    show_completed = request.GET.get("show-completed") == "true"
-    show_failed = request.GET.get("show-failed", "true") == "true"
-
+def _query_jobs(show_completed: bool, show_failed: bool):
     jobs = MailoutJob.objects.all().order_by("-id")
 
     if not show_completed:
@@ -45,11 +41,54 @@ def jobs_list(request) -> HttpResponse:
         if not show_failed:
             jobs = jobs.exclude(state=MailoutJob.SendState.FAILED)
 
+    return jobs
+
+
+
+@permission_required("toolkit.read")
+@require_GET
+def jobs_list(request) -> HttpResponse:
+    show_completed = False
+    show_failed = True
+    jobs = _query_jobs(show_completed=show_completed, show_failed=show_failed)
+
+    poll_for_updates = any(
+        job.state
+        in (
+            MailoutJob.SendState.PENDING,
+            MailoutJob.SendState.SENDING,
+            MailoutJob.SendState.CANCELLING,
+        )
+        for job in jobs
+    )
+
     return render(
         request,
         "jobs-list.html",
         context={
             "jobs": jobs,
+            "poll_for_updates": poll_for_updates,
+            "show_completed": show_completed,
+            "show_failed": show_failed,
+        },
+    )
+
+
+@permission_required("toolkit.read")
+@require_GET
+def jobs_table(request) -> HttpResponse:
+    show_completed = request.GET.get("show-completed") == "on"
+    show_failed = request.GET.get("show-failed", "true") == "on"
+    poll_for_updates = request.GET.get("poll-for-updates") == "on"
+
+    jobs = _query_jobs(show_completed=show_completed, show_failed=show_failed)
+
+    return render(
+        request,
+        "jobs-list-table.html",
+        context={
+            "jobs": jobs,
+            "poll_for_updates": poll_for_updates,
             "show_completed": show_completed,
             "show_failed": show_failed,
         },
