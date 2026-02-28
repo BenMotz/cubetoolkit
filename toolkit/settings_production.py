f287cc92f3dfaa12f5c2a65d234fa33f720e4504
Ben Motz <ben@ouat.co.uk>
2026-02-28 11:10:19 +0000

    Use TLS for email

--- toolkit/settings_production.py
+++ toolkit/settings_production.py
@@ -48,6 +48,7 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "https://cubecinema.com",
