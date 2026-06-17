ce457c9dd0bde5505b2885af795316e650f77e8d
Ben Motz <ben@ouat.co.uk>
2026-06-17 20:15:41 +0100

    Format JS

--- toolkit/members/static/members/js/webcam.js
+++ toolkit/members/static/members/js/webcam.js
@@ -3,125 +3,125 @@ var setupPage = function() {
     /* 'Derived' from:
      * https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API/Taking_still_photos
      */
-    let streaming        = false;
-    const video            = document.getElementById('video');
-    const canvas           = document.getElementById('canvas');
-    const photo            = document.getElementById('photo');
+    let streaming = false;
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const photo = document.getElementById('photo');
     const originalPhotoSrc = photo.getAttribute('src');
-    const captureButton    = document.getElementById('capturebutton');
+    const captureButton = document.getElementById('capturebutton');
     const enableCaptureButton = document.getElementById('enablecapturebutton');
     const width = 320;
     let height = 0;
 
-  if (window.self !== window.top) {
-    // If our document is in a frame we won't be able to request permission
-    // for camera access.
-    console.error("In a frame, cannot use the camera");
-    return;
-  }
-
-  if (navigator.mediaDevices !== undefined) {
-    enableCaptureButton.style.display = 'initial';
-  } else {
-    console.log("WebRTC not supported");
-    return;
-  }
-
-  // Fill the photo with an indication that none has been
-  // captured.
-
-  function clearphoto() {
-    photo.setAttribute("src", originalPhotoSrc);
-  }
-
-  function showPreview() {
-      document.getElementById('webcampreview').style.display = 'initial';
-  }
-
-  function hidePreview() {
-      document.getElementById('webcampreview').style.display = 'none';
-  }
-
-  function setError(msg) {
-      document.getElementById('webcamerror').textContent = msg;
-  }
-
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
-
-  function takePicture() {
-    const context = canvas.getContext("2d");
-    if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
-
-      const data = canvas.toDataURL("image/png");
-      photo.setAttribute("src", data);
-      document.getElementById('id_vol-image_data').value = data;
-    } else {
-      clearphoto();
+    if(window.self !== window.top) {
+        // If our document is in a frame we won't be able to request permission
+        // for camera access.
+        console.error("In a frame, cannot use the camera");
+        return;
     }
-  }
 
-  function startCapture() {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then((stream) => {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch((err) => {
-          if (err.name === "NotFoundError") {
-            setError(`No suitable camera found!`);
-          } else {
-            setError(`An error occurred: ${err}`);
-          }
-          hidePreview();
-      });
+    if(navigator.mediaDevices !== undefined) {
+        enableCaptureButton.style.display = 'initial';
+    } else {
+        console.log("WebRTC not supported");
+        return;
+    }
 
-    video.addEventListener(
-      "canplay",
-      (ev) => {
-        if (!streaming) {
-          height = video.videoHeight / (video.videoWidth / width);
+    // Fill the photo with an indication that none has been
+    // captured.
 
-          // Firefox currently has a bug where the height can't be read from
-          // the video, so we will make assumptions if this happens.
+    function clearphoto() {
+        photo.setAttribute("src", originalPhotoSrc);
+    }
 
-          if (isNaN(height)) {
-            height = width / (4 / 3);
-          }
+    function showPreview() {
+        document.getElementById('webcampreview').style.display = 'initial';
+    }
 
-          video.setAttribute("width", width);
-          video.setAttribute("height", height);
-          canvas.setAttribute("width", width);
-          canvas.setAttribute("height", height);
-          streaming = true;
+    function hidePreview() {
+        document.getElementById('webcampreview').style.display = 'none';
+    }
+
+    function setError(msg) {
+        document.getElementById('webcamerror').textContent = msg;
+    }
+
+    // Capture a photo by fetching the current contents of the video
+    // and drawing it into a canvas, then converting that to a PNG
+    // format data URL. By drawing it on an offscreen canvas and then
+    // drawing that to the screen, we can change its size and/or apply
+    // other changes before drawing it.
+
+    function takePicture() {
+        const context = canvas.getContext("2d");
+        if(width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(video, 0, 0, width, height);
+
+            const data = canvas.toDataURL("image/png");
+            photo.setAttribute("src", data);
+            document.getElementById('id_vol-image_data').value = data;
+        } else {
+            clearphoto();
         }
-      },
-      false
-    );
+    }
 
-    captureButton.addEventListener(
-      "click",
-      (ev) => {
-        takePicture();
+    function startCapture() {
+        navigator.mediaDevices
+            .getUserMedia({ video: true, audio: false })
+            .then((stream) => {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch((err) => {
+                if(err.name === "NotFoundError") {
+                    setError(`No suitable camera found!`);
+                } else {
+                    setError(`An error occurred: ${err}`);
+                }
+                hidePreview();
+            });
+
+        video.addEventListener(
+            "canplay",
+            (ev) => {
+                if(!streaming) {
+                    height = video.videoHeight / (video.videoWidth / width);
+
+                    // Firefox currently has a bug where the height can't be read
+                    // from the video, so we will make assumptions if this happens.
+
+                    if(isNaN(height)) {
+                        height = width / (4 / 3);
+                    }
+
+                    video.setAttribute("width", width);
+                    video.setAttribute("height", height);
+                    canvas.setAttribute("width", width);
+                    canvas.setAttribute("height", height);
+                    streaming = true;
+                }
+            },
+            false
+        );
+
+        captureButton.addEventListener(
+            "click",
+            (ev) => {
+                takePicture();
+                ev.preventDefault();
+            },
+            false
+        );
+
+        clearphoto();
+    }
+
+    enableCaptureButton.addEventListener('click', function(ev) {
         ev.preventDefault();
-      },
-      false
-    );
-
-    clearphoto();
-  }
-
-  enableCaptureButton.addEventListener('click', function(ev) {
-      ev.preventDefault();
-      showPreview();
-      enableCaptureButton.style.display = 'none';
-      startCapture();
-  });
+        showPreview();
+        enableCaptureButton.style.display = 'none';
+        startCapture();
+    });
 };
